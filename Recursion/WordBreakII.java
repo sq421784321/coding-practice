d75c1280736abb1d9aa86637cc19523258c7bbe8
Neil-haha <quansun1993@gmail.com>
2017-08-23 15:36:45 -0700

    Update WordBreakII.java

--- Recursion/WordBreakII.java
+++ Recursion/WordBreakII.java
@@ -1,3 +1,13 @@
+// Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. You may assume the dictionary does not contain duplicate words.
+
+// Return all such possible sentences.
+
+// For example, given
+// s = "catsanddog",
+// dict = ["cat", "cats", "and", "sand", "dog"].
+
+// A solution is ["cats and dog", "cat sand dog"].
+
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         return helper(s, wordDict, new HashMap<String, List<String>>());
