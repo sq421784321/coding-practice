f7c27e3046f2014ed81974cdb67f831f939e2e32
Neil-haha <quansun1993@gmail.com>
2017-08-23 13:44:59 -0700

    Update PermutationSequence.java

--- Math/PermutationSequence.java
+++ Math/PermutationSequence.java
@@ -1,3 +1,17 @@
+// The set [1,2,3,…,n] contains a total of n! unique permutations.
+
+// By listing and labeling all of the permutations in order,
+// We get the following sequence (ie, for n = 3):
+
+// "123"
+// "132"
+// "213"
+// "231"
+// "312"
+// "321"
+// Given n and k, return the kth permutation sequence.
+
+
 class Solution {
     public String getPermutation(int n, int k) {
         List<Integer> list = new ArrayList<>();
