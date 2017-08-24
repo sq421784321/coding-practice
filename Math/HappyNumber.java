cfd4d1d2ff784b3725a9f2448868a667125f79f5
Neil-haha <quansun1993@gmail.com>
2017-08-24 00:38:49 -0700

    Update HappyNumber.java

--- Math/HappyNumber.java
+++ Math/HappyNumber.java
@@ -1,16 +1,16 @@
-Write an algorithm to determine if a number is "happy".
+// Write an algorithm to determine if a number is "happy".
 
-A happy number is a number defined by the following process: Starting with any positive integer, 
-replace the number by the sum of the squares of its digits, 
-and repeat the process until the number equals 1 (where it will stay), 
-or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+// A happy number is a number defined by the following process: Starting with any positive integer, 
+// replace the number by the sum of the squares of its digits, 
+// and repeat the process until the number equals 1 (where it will stay), 
+// or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
-Example: 19 is a happy number
+// Example: 19 is a happy number
 
-1 * 1 + 9 * 9 = 82
-8 * 8 + 2 * 2 = 68
-6 * 6 + 8 * 8 = 100
-1 * 1 + 0 * 0 + 0 * 0 = 1
+// 1 * 1 + 9 * 9 = 82
+// 8 * 8 + 2 * 2 = 68
+// 6 * 6 + 8 * 8 = 100
+// 1 * 1 + 0 * 0 + 0 * 0 = 1
 
 class Solution {
     public boolean isHappy(int n) {
