9820ee751067ebf5d4319841e4eeec2ca0ade938
Quan Sun <quansun1993@gmail.com>
2017-09-21 20:11:24 -0700

    Create HouseRobber.java

---
+++ DynamicProgramming/HouseRobber.java
@@ -0,0 +1,19 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+*/
+
+
+class Solution {
+    // f(i) = max(A[i] + f(i - 2), f(i - 1))
+    public int rob(int[] nums) {
+        int prev = 0, cur = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int tmp = cur;
+            cur = Math.max(prev + nums[i], cur);
+            prev = tmp;
+        }
+        return cur;
+    }
+}
