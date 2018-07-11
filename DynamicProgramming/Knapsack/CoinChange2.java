41aad1148ffc2cee4a22877755be31eb4d4a16ad
Neil-Haha <quansun@usc.edu>
2018-07-11 16:27:16 -0700

    Solution for CoinChange2

---
+++ DynamicProgramming/Knapsack/CoinChange2.java
@@ -0,0 +1,22 @@
+Cass Solution {
+    public int change(int amount, int[] coins) {
+            // dp[i][j] : the number of unique combinations to make up to i by using only the first j types of coin 
+	            int[][] dp = new int[coins.length + 1][amount + 1];
+		            dp[0][0] = 1;
+			            
+				            for (int i = 1; i <= coins.length; i++) {
+					                dp[i][0] = 1;
+							            for (int j = 0; j <= amount; j++) {
+								                    dp[i][j] = dp[i-1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+										                }
+												        }
+													        
+														        return dp[coins.length][amount];
+															    }
+															        
+																    // dp[1] = 1 = 1
+																        // dp[2] = 1+1/2 = 2
+																	    // dp[3] = 1+1+1/1+2 = 2
+																	        // dp[4] = 1+1+1+1/1+1+2/2+2 = 3
+																		    // dp[5] = 5/1+1+1+1+1/1+1+1+2/1+2+2 = 4
+																		    }
