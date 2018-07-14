bb4f5ed372b8fd38ef05b78a7d0b617a9ced5dc4
Neil-haha <quansun@usc.edu>
2018-07-14 15:58:15 -0700

    add bfs sol

--- Graph/DFS/417-Pacific-Atlantic-Water-Flow.java
+++ Graph/DFS/417-Pacific-Atlantic-Water-Flow.java
@@ -44,3 +44,74 @@ class Solution {
         }
     }
 }
+
+
+/* Method2 BFS
+class Solution {
+    private int[][] dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+    
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return new ArrayList<int[]>();
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        boolean[][] canTouchPacific = new boolean[n][m];
+        boolean[][] canTouchAtlantic = new boolean[n][m];
+        List<int[]> ans = new ArrayList<>();
+        
+        Deque<int[]> pQueue = new LinkedList<>();
+        Deque<int[]> aQueue = new LinkedList<>();
+        
+        for (int i = 0; i < n; i++) {
+            pQueue.offer(new int[] {i, 0});
+            aQueue.offer(new int[] {i, m - 1});
+            canTouchPacific[i][0] = true;
+            canTouchAtlantic[i][m - 1] = true;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            pQueue.offer(new int[] {0, i});
+            aQueue.offer(new int[] {n - 1, i});
+            canTouchPacific[0][i] = true;
+            canTouchAtlantic[n - 1][i] = true;
+        }
+        
+        bfs(matrix, pQueue, canTouchPacific);
+        bfs(matrix, aQueue, canTouchAtlantic);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (canTouchPacific[i][j] && canTouchAtlantic[i][j]) {
+                    ans.add(new int[]{i, j});
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    private void bfs(int[][] matrix, Deque<int[]> q, boolean[][] visited) {
+        int n = matrix.length, m = matrix[0].length;
+        
+        while (!q.isEmpty()) {
+            int[] cur = q.pollFirst();
+            int row = cur[0];
+            int col = cur[1];
+            visited[row][col] = true;
+            
+            for (int i = 0; i < dirs.length; i++) {
+                int newRow = row + dirs[i][0];
+                int newCol = col + dirs[i][1];
+                
+                if (newRow < 0 || newRow >= n || newCol < 0 || newCol >= m || visited[newRow][newCol] || matrix[newRow][newCol] < matrix[row][col]) {
+                    continue;
+                }
+                
+                q.offer(new int[] {newRow, newCol});
+            }
+        }
+    }
+}
+
+*/
