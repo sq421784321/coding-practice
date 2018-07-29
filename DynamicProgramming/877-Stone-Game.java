8d6086a53d87c7d9b4c16310f7db54027029e26e
Neil-haha <quansun@usc.edu>
2018-07-29 14:36:16 -0700

    use difference as dp array

---
+++ DynamicProgramming/877-Stone-Game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        
+        // dp[i][j] : the biggest number you can get more than your opponent in the range piles[i, j]
+        // dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+        // 
+        // base case: dp[i][i] = piles[i]
+        
+        int[][] dp = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        
+        return dp[0][n - 1] > 0;
+    }
+}
