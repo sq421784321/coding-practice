28879df28c148e9a29ff56fcb06c71de26bcda7a
Neil-haha <quansun@usc.edu>
2018-08-10 16:46:42 -0700

    left cumulative sum & right cumulative sum

--- Array/724-Find-Pivot-Index.java
+++ Array/724-Find-Pivot-Index.java
@@ -1,1 +1,27 @@
-afas
+class Solution {
+	/*
+		Method 1: Iterative
+		
+	*/
+	
+    public int pivotIndex(int[] nums) {
+        int right = 0;
+        for (int num : nums) {
+            right += num;
+        }
+        
+        int left = 0;
+        for (int i = 0; i < nums.length; i++) {
+            
+            right -= nums[i];
+            if (i > 0) {
+                left += nums[i - 1];
+            }
+            
+            if (left == right) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
