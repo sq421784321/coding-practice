36f7b4e437b7339676763842d0014d010ff019c0
Neil-haha <quansun@usc.edu>
2018-08-11 23:38:53 -0700

    hashmap to store counts of words in A and B.

--- HashTable/888-Uncommon-Words-From-Two-Sentences.java
+++ HashTable/888-Uncommon-Words-From-Two-Sentences.java
@@ -1,33 +1,31 @@
 class Solution {
+	/*
+		Method 1: HashTable
+		
+		Iterate through all words in A and B, construct freq map
+		Put those non-empty words of 1 freq into final list.
+	*/
     public String[] uncommonFromSentences(String A, String B) {
         List<String> ans = new ArrayList<>();
         
-        Map<String, Integer> mapA = new HashMap<>();
-        Map<String, Integer> mapB = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         
         String[] wordsA = A.split(" ");
         String[] wordsB = B.split(" ");
         
         for (String word: wordsA) {
-            mapA.put(word, mapA.getOrDefault(word, 0) + 1);
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
         
         for (String word: wordsB) {
-            mapB.put(word, mapB.getOrDefault(word, 0) + 1);
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
         
-        for (String word: mapA.keySet()) {
-            if (word.length() > 0 && mapA.get(word) == 1 && !mapB.containsKey(word)) {
+        for (String word: map.keySet()) {
+            if (word.length() > 0 && map.get(word) == 1) {
                 ans.add(word);
             }
         }
-        
-        for (String word: mapB.keySet()) {
-            if(word.length() > 0 && mapB.get(word) == 1 && !mapA.containsKey(word)) {
-                ans.add(word);
-            }
-        }
-        
         
         String[] array = new String[ans.size()];
         for (int i = 0; i < ans.size(); i++) {
