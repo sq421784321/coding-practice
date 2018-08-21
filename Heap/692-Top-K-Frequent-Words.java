3a847625e3fd80dccecd272318c231d0f9dc54be
Neil-haha <quansun@usc.edu>
2018-08-21 10:51:54 -0700

    minHeap

--- Heap/692-Top-K-Frequent-Words.java
+++ Heap/692-Top-K-Frequent-Words.java
@@ -1,51 +1,44 @@
 class Solution {
-    public List<String> topKFrequent(String[] words, int k) {
+    /*
+        Method 1: PriorityQueue
         
-        // Time: O(n), Space: O(n)
+        Time: O(n * log k), Space: O(n)
+    */
+    
+    public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> freqMap = getMap(words);
         
-        // Create a minHeap to store map entry
+        // minHeap to sort map entries in ascending order (freq small first, alphabetical large first)
         PriorityQueue<Map.Entry<String, Integer>> minHeap = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>(){
             @Override
             public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
                 if (e1.getValue() != e2.getValue()) {
                     return e1.getValue() - e2.getValue();
-                } else {
-                    return e2.getKey().compareTo(e1.getKey());
                 }
+                return e2.getKey().compareTo(e1.getKey());
             }
         }); 
         
-        // Time: O(n * logk)
         for (Map.Entry<String, Integer> cur: freqMap.entrySet()) {
-            if (minHeap.size() < k) {
-                minHeap.offer(cur);
-            } else {
-                if (cur.getValue() > minHeap.peek().getValue()){
-                    minHeap.poll();
-                    minHeap.offer(cur);
-                } else if (cur.getValue() == minHeap.peek().getValue() && cur.getKey().compareTo(minHeap.peek().getKey()) < 0) {
-                    minHeap.poll();
-                    minHeap.offer(cur);
-                }
+            minHeap.add(cur);
+            if (minHeap.size() > k) {
+                minHeap.poll();
             }
         }
         
-        //List<String> ans = new ArrayList<>();
+        // Append from the rightmost position
         String[] array = new String[minHeap.size()];
-        for (int i = minHeap.size() - 1; i >= 0; i--) {
-            array[i] = minHeap.poll().getKey();
+        while (!minHeap.isEmpty()) {
+            array[minHeap.size() - 1] = minHeap.poll().getKey();
         }
         return Arrays.asList(array);
     }
     
     private Map<String, Integer> getMap(String[] words) {
         Map<String, Integer> map = new HashMap<>();
-        
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        
         return map;
     }
 }
