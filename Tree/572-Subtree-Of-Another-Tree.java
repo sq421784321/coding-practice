792c31a8e9348cf4d36f770e9cdd3364a1bc5435
Neil-haha <quansun@usc.edu>
2018-08-23 14:53:53 -0700

    greedy

---
+++ Tree/572-Subtree-Of-Another-Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    /*
+		Method 1: Recursion.
+		
+		Recursively check, whenever s.val == t.val, we call isSame to check the equality of two trees.
+	*/
+	
+	public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else if (s.val != t.val) {
+            return false;
+        }
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
