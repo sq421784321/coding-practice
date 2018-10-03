7f43f29d4bbe3c644e56459fb2e649b443d3dde1
Neil-haha <quansun@usc.edu>
2018-10-02 22:37:13 -0700

    binary search, be careful of terminate condition & reduce rule

---
+++ BinarySearch/704-Binary-Search.java
@@ -0,0 +1,23 @@
+class Solution {
+	/*
+		Method 1: Traditional Binary Search.
+	*/
+	
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
